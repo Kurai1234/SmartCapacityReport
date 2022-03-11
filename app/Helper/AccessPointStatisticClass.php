0c1b14903324efdd2e3f1e45353973e192ed66fb
Kurai1234 <hectorcaste94@gmail.com>
2022-03-11 10:01:01 -0600

    working so far without pagin

--- app/Helper/AccessPointStatisticClass.php
+++ app/Helper/AccessPointStatisticClass.php
@@ -3,7 +3,7 @@
 use PhpParser\Builder\Class_;
 use App\Models\Maestro;
 use GuzzleHttp\Client;
-
+use Ramsey\Uuid\Type\Integer;
 
 class AccessPointStatisticHelperClass
 {
@@ -14,14 +14,16 @@ class AccessPointStatisticHelperClass
     public string $url;
     public string $page;
     public string $filter;
-    public array $reponse_data;
-    function __construct(String $maestro, String $clientId, String $clientSecret,String $api_append)
+    public int $paging;
+    public array $response_data;
+    function __construct(String $maestro, String $clientId, String $clientSecret, String $api_append)
     {
         $this->maestro = $maestro;
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
         $this->api_append = $api_append;
-        $this->set_url_query(array('mode'=>'ap'));
+        // $this->response_data;
+        $this->set_url_query(array());
     }
     function get_maestro()
     {
@@ -59,19 +61,50 @@ class AccessPointStatisticHelperClass
     function call_api()
     {
         $api_request = new Client(['verify' => false]);
-        $api_raw_response = $api_request->get($this->maestro . $this->api_append.'?'.$this->filter,[
+        $api_raw_response = $api_request->get($this->maestro . $this->api_append . '?' . $this->filter, [
             'headers' => [
                 'Authorization' => 'Bearer ' . $this->get_token(),
             ]
         ]);
         $api_data = json_decode($api_raw_response->getBody()->getContents());
-        $this->reponse_data= $api_data->data;
+        $this->response_data= $api_data->data;
+        $this->paging = $api_data->paging->total;
+
+        // if ($api_data->paging->total > $api_data->paging->limit) {
+        //     $api_object_total = $api_data->paging->total;
+        //     $api_objects_limit = $api_data->paging->limit;
+        //     for ($counter = $api_objects_limit; $counter < $api_object_total; $counter += $api_objects_limit) {
+        //         $api_url_query = array(
+        //             'mode' => 'ap',
+        //             'offset' => $counter,
+
+        //         );
+        //         $this->set_url_query($api_url_query);
+        //         // $api_new_query = http_build_query($api_url_query);
+
+        //         $api_raw_response = $api_request->get(env('MAESTRO_SECOND_SERVER') . '/devices/statistics?' . $this->filter, [
+        //             'headers' => [
+        //                 'Authorization' => 'Bearer ' . $this->get_token(),
+        //             ]
+        //         ]);
+        //         $api_data->data = array_merge($api_data->data, json_decode($api_raw_response->getBody()->getContents())->data);
+        //     }
+        // }
+
+
+
+        return $api_data;
     }
 
 
     function get_response_data()
     {
-        return $this->reponse_data;
+        // $this->call_api();
+
+        return $this->response_data;
+    }
+    function get_paging(){
+        return $this->paging;
     }
 }
 
--- app/Http/Controllers/AccesspointController.php
+++ app/Http/Controllers/AccesspointController.php
@@ -15,9 +15,20 @@ class AccesspointController extends Controller
     {
         // $testing= get_maestro_api_token();
         $testing = new AccessPointStatisticHelperClass(env('MAESTRO_SECOND_SERVER'),env('CLIENT_ID_SECOND'),env('CLIENT_SECRET_SECOND'),'/devices/statistics');
+        $testing2 = new AccessPointStatisticHelperClass(env('MAESTRO_SECOND_SERVER'),env('CLIENT_ID_SECOND'),env('CLIENT_SECRET_SECOND'),'/devices/statistics');
+
         // $testing->get_maestro();
+        // $testing->call_api();
+        $testing->set_url_query(array('mode'=>'ap'));
+        $testing2->set_url_query(array('mode'=>'ap','offset'=>'100'));
+
         $testing->call_api();
-        dd($testing->get_response_data());
+        $testing2->call_api();
+        $final = array_merge($testing->get_response_data(),$testing2->get_response_data());
+        // dd($testing->call_api());
+        // dd($testing->get_paging());
+        // dd($testing->get_response_data());
+        dd($final);
         $testing = AccessPoint::with(['tower','tower.network','tower.network.maestro'])->findOrFail(1);
 
         return view('auth.pages.accesspoint', ['testing' => $testing]);
