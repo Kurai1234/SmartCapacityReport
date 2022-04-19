92a3320a22ede8576aefd1c6d503fd5256055b7f
Hector Castellanos <hectorcaste94@gmail.com>
2022-04-19 08:02:52 -0600

    latest refractor

--- config/database.php
+++ config/database.php
@@ -60,6 +60,7 @@ return [
             'engine' => null,
             'dump' => [
                 'dump_binary_path' => 'C:/xampp/mysql/bin/', // only the path, so without `mysqldump` or `pg_dump`
+                //dump_command_path for linux
 
              ],
             'options' => extension_loaded('pdo_mysql') ? array_filter([
